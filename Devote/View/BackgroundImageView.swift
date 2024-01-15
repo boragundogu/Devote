74c754b187791b39e28ce4f0a827523026e32291
Bora <gundogubora24@gmail.com>
2024-01-15 23:54:26 +0300

    changing contentview design

---
+++ Devote/View/BackgroundImageView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundImageView.swift
+//  Devote
+//
+//  Created by Bora Gündoğu on 15.01.2024.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundImageView()
+}
