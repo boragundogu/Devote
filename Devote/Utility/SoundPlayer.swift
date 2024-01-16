bd304e6d532b69ac1d9ccec98ca5d816e9f66205
Bora <gundogubora24@gmail.com>
2024-01-16 22:44:31 +0300

    custom header, TaskItemView, custom visual effect, custom checkbox,  ux and sound design

---
+++ Devote/Utility/SoundPlayer.swift
@@ -0,0 +1,8 @@
+//
+//  SoundPlayer.swift
+//  Devote
+//
+//  Created by Bora Gündoğu on 16.01.2024.
+//
+
+import Foundation
---
+++ Devote/View/BlankView.swift
@@ -0,0 +1,18 @@
+//
+//  BlankView.swift
+//  Devote
+//
+//  Created by Bora Gündoğu on 16.01.2024.
+//
+
+import SwiftUI
+
+struct BlankView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlankView()
+}
